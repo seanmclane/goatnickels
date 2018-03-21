83f0c51776f7d0fb10ae208cb5b7aaa3c9c11710
Sean McLane <smclane@Seans-MacBook-Pro-2.local>
2018-03-21 14:52:26 -0600

    testing

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM golang
 RUN go get github.com/seanmclane/goatnickels
 RUN cd src/github.com/seanmclane/goatnickels
 RUN mkdir /goatchain
-RUN echo "{\"directory\":\"/goatchain\",\"nodes\": [\"goat1\",\"goat2\",\"goat3\"]}" > config.json
+RUN echo "{\"directory\":\"/goatchain/\",\"nodes\": [\"goat1\",\"goat2\",\"goat3\"]}" > config.json
 EXPOSE 3000
 RUN goatnickels -genesis y
 CMD ["goatnickels", "-serve", "y"]
--- block/block.go
+++ block/block.go
@@ -166,11 +166,15 @@ func InitializeState() {
   //TODO: if no blockchain, get it from the network instead
   config := LoadConfig()
 
+  var reqClient = &http.Client{
+      Timeout: time.Second * 10,
+    }
+
   var max_list []int64
   for key, node := range config.Nodes {
     max_list = append(max_list, 0)
     time.Sleep(5 * time.Second)
-    r, err := http.Get("http://"+node+":3000/api/v1/maxblock")
+    r, err := reqClient.Get("http://"+node+":3000/api/v1/maxblock")
     if err != nil {
       fmt.Println("no respnse from node:", node)
     } else {
