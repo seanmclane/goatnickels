b390998eab656230d9c618854f3fb9726acb1e13
Sean McLane <smclane@Seans-MacBook-Pro-2.local>
2018-03-22 11:21:17 -0600

    maybe I should use the right private key

--- block/block.go
+++ block/block.go
@@ -381,8 +381,8 @@ func SendVoteToNetwork() {
     Account: "goat_04dbb67ae9650ca3258071909f74be5400fe53fc2e5dcc82103020f3aeefeee5f9980c4c05bb8696215458dfa7ddaa1505d2826cab3d246b8930b0694f766a22f8bb63932368c0b12bf80cfaee8a18db1d7ce19df0a84215d20b0bbfbd30d95c25", //TODO: add account key to config for each node
     Hash: "20bea146cc483a81a482b5b93228d7856cb1656161d7409ab0ac87b35ed3cc91f8a67c1c0d5c89a9c832395ff260ea07c2388f6b8503bbd123047b4dd35eeaff",
     Signature: Signature {
-      R: "9b723e12925e535e763529f4f538e03aa90b82f198d588f8b024117d79580c9d98a63575b9e1b0142c62b66777a15e62",
-      S: "d3d9064dfebd640ab03c471625d62c5cec53b6d98eea874efbed522dd317038ee35e25105749cf124b3a759c636dc807",
+      R: "d0fafa4169d01575a9b868994ea51a1bbcfd3d2b4c6fc414002ee32f1f9cf551e1b90aee0bee5834f54d321056b57a22",
+      S: "8d1dfa614df344c4011e26b5abb3b509586f2cfb1999ded0c0f256e064b2d0ea11bfd96bbce4d76da5d1a0239cd1620a",
       }, //TODO: actually create a signature, which requires the private key be kept on the server
   }
 
